c95c26156e4d708f17a21119aef866b88deea980
Unknown <Kr4kenKT@gmail.com>
2017-09-15 15:13:30 -0500

    Working Project

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace PastebinRipper
+{
+    internal class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Insert raw pastebin link.");
+
+            String URL = Console.ReadLine();
+            
+            Console.WriteLine("Insert path to save to.");
+
+            String Path = Console.ReadLine();
+
+            String[] URLs;
+
+            using (WebClient Client = new WebClient())
+                URLs = Client.DownloadString(new Uri(URL)).Split('\n');
+
+            int Downloaded = 0;
+
+            Parallel.For(0, URLs.Length, index =>
+            {
+                using (WebClient Client = new WebClient())
+                {
+                    String[] URLPieces = URLs[index].Split('.');
+
+                    String Name = URLPieces[2].TrimStart('i', 't', 'c', 'o', 'm', '/') + $".{URLPieces[3].TrimEnd('\r')}";
+
+                    Client.DownloadFile(new Uri(URLs[index]), $"{Path}{Name}");
+
+                    Downloaded++;
+                    
+                    Console.WriteLine($"Progress: {Downloaded} of {URLs.Length}");
+                }
+            });
+        }
+    }
+}
